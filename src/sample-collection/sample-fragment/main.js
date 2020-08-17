1adc2f14b292c4e51529a1c997b3cd768083b895
Iván Zaera Avellón <ivan.zaera@liferay.com>
2020-08-17 13:12:10 +0200

    Change component to follow DXP patterns

--- src/sample-collection/sample-fragment/main.js
+++ src/sample-collection/sample-fragment/main.js
@@ -1,8 +1,5 @@
 import React from "react";
-import ReactDOM from "react-dom";
 
-export default class extends React.Component {
-  render() {
-    return <div>Holi!</div>;
-  }
+export default function() {
+  return <div>Holi!</div>;
 }
